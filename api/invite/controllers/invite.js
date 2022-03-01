b53d8170c5a7760724d924e4674f58dfb5eb2c3d
itai rozen <itai.rozen@mail.huji.ac.il>
2022-03-01 14:21:29 +0200

    added aility to add & delete invite to an event

--- api/invite/controllers/invite.js
+++ api/invite/controllers/invite.js
@@ -1,8 +1,48 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.invite.create(data, { files });
+    } else {
+      entity = await strapi.services.invite.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.invite });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.invite.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.invite });
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.invite.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.invite.update({ id }, ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.invite });
+  },
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.invite.findOne({ id });
+    return sanitizeEntity(entity, { model: strapi.models.invite });
+  },
+};
+ 
